52e5fb2e5626290d3ff73620323a9a1228060e61
Ian Cabigon <iancabigon@gmail.com>
2018-09-04 17:51:45 +0800

    inital implementation

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:qr_reader/qr_reader.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'QR Reader',
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  final Map<String, dynamic> pluginParameters = {
+  };
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _barcodeString;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('QR Reader'),
+      ),
+      body: new Center(
+          child: new FutureBuilder<String>(
+              future: _barcodeString,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+
+                if( snapshot.data != null ){
+                  final String toLaunch = snapshot.data;
+                  _launched = _launchInBrowser(toLaunch);
+                }
+                
+                return _launchStatus(context, snapshot);
+              })),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _barcodeString = new QRCodeReader()
+                .setAutoFocusIntervalInMs(200)
+                .setForceAutoFocus(true)
+                .setTorchEnabled(true)
+                .setHandlePermissions(true)
+                .setExecuteAfterPermissionGranted(true)
+                .scan();
+          });
+        },
+        tooltip: 'Reader the QRCode',
+        child: new Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  Future<Null> _launched;
+
+  Future<Null> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Future<Null> _launchInWebViewOrVC(String url) async {
+  //   if (await canLaunch(url)) {
+  //     await launch(url, forceSafariVC: true, forceWebView: true);
+  //   } else {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<String> snapshot) {
+    if (snapshot.hasError) {
+      return new Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+}
